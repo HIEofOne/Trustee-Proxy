a08c68cf4075452fb0faa4b23d08d7a4c5bd56b1
Michael Shihjay Chen <shihjay2@gmail.com>
2024-12-29 10:30:59 -0800

    debug

--- veramo.mjs
+++ veramo.mjs
@@ -18,18 +18,18 @@ import fs from 'fs'
 const INFURA_PROJECT_ID = process.env.INFURIA_API_KEY
 let KMS_SECRET_KEY = null
 try {
-  KMS_SECRET_KEY = fs.readFileSync('./kms', 'utf8')
+  KMS_SECRET_KEY = fs.readFileSync('./proxy/kms', 'utf8')
   console.log(KMS_SECRET_KEY)
 } catch (err) {
   KMS_SECRET_KEY = await SecretBox.createSecretKey()
   console.log(KMS_SECRET_KEY)
   KMS_SECRET_KEY = 'cd198a03032e8d072066e3195b8e1e176e78b5cfbb378a5fc00ae4ded5c002e9'
-  fs.writeFileSync('./kms', KMS_SECRET_KEY)
+  fs.writeFileSync('./proxy/kms', KMS_SECRET_KEY)
 }
 
 const dbConnection = new DataSource({
   type: 'sqlite',
-  database: './database.sqlite',
+  database: './proxy/database.sqlite',
   synchronize: false,
   migrations,
   migrationsRun: true,
